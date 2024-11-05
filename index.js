d9c65be7ddf2da010a1cdea970291107faf8e321
Vallabh0440 <Vallabh0440@gmail.com>
2024-11-05 12:05:14 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector ("game-info");
+const newGameBtn = document.querySelector (".btn");
+
+
+let currentPlayer;
+let gameGridid;
+
+const winningPositions=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+//function to initialise game
+function initGame(){
+    currentPlayer = "X";
+    gameGrid = ["","","","","","","","",""];
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText = `current player - ${currentPlayer}`
+
+}
+
+initGame();
+
+boxes.forEach((box,index) => {
+    box.addEventListener("click",()=>{
+        handleClick(index);
+    })
+});
+
