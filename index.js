a058434b115772e613b5a577937d34fd4a26ee29
Vallabh0440 <Vallabh0440@gmail.com>
2024-11-06 14:52:36 +0530

    new commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const boxes = document.querySelectorAll(".box");
-const gameInfo = document.querySelector ("game-info");
+const gameInfo = document.querySelector (".game-info");
 const newGameBtn = document.querySelector (".btn");
 
 
 let currentPlayer;
-let gameGridid;
+let gameGrid;
+
 
 const winningPositions=[
     [0,1,2],
@@ -21,12 +22,39 @@ const winningPositions=[
 function initGame(){
     currentPlayer = "X";
     gameGrid = ["","","","","","","","",""];
+    boxes.forEach((box,index)=>{
+        box.innerText="";
+        boxes[index].computedStyleMap.pointerEvents = "all";
+    }); 
     newGameBtn.classList.remove("active");
-    gameInfo.innerText = `current player - ${currentPlayer}`
+    gameInfo.innerText = `current player - ${currentPlayer}`;
+}
+initGame();
 
+function swapTurn(){
+    if (currentPlayer ==="X"){
+        currentPlayer = "O";
+    }
+    else{
+        currentPlayer = "X";
+    }
+    gameInfo.innerText = `current player - ${currentPlayer}`;
 }
 
-initGame();
+function checkGameOver(){
+    newGameBtn.classList.add("active")
+}
+
+function handleClick(index){
+    if(gameGrid[index] === "") {
+        boxes[index].innerHTML = currentPlayer;
+        gameGrid[index] = currentPlayer;
+        //swap turn
+        swapTurn();
+        //check game over or not
+        checkGameOver();
+    }
+}
 
 boxes.forEach((box,index) => {
     box.addEventListener("click",()=>{
