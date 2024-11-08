c6328700f61128d3a485453851b7bb57115d39ee
Vallabh0440 <Vallabh0440@gmail.com>
2024-11-08 17:22:22 +0530

    new commit

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const newGameBtn = document.querySelector (".btn");
 let currentPlayer;
 let gameGrid;
 
-
 const winningPositions=[
     [0,1,2],
     [3,4,5],
@@ -28,7 +27,7 @@ function initGame(){
     }); 
     newGameBtn.classList.remove("active");
     gameInfo.innerText = `current player - ${currentPlayer}`;
-}
+};
 initGame();
 
 function swapTurn(){
@@ -39,26 +38,53 @@ function swapTurn(){
         currentPlayer = "X";
     }
     gameInfo.innerText = `current player - ${currentPlayer}`;
-}
+};
 
 function checkGameOver(){
-    newGameBtn.classList.add("active")
-}
+    newGameBtn.classList.add("active");
+    let answer = "";
+
+    winningPositions.forEach((position) => {
+        if( (gameGrid[position[0]] !=="" || gameGrid[position[1]] !=="" || gameGrid[position[2]] !== "") && (gameGrid[position[0]] === gameGrid[position[1]]&& gameGrid[position[1]] ===  gameGrid[position[2]])){
+
+        if(gameGrid[position[0]] === "X")
+                answer = "X";
+            else
+            answer = "O";
+
+            boxes.forEach((box)=>{
+                box.style.pointerEvents = "none "
+            })
+            
+            boxes[position[0]].classList.add("win");
+            boxes[position[1]].classList.add("win");
+            boxes[position[2]].classList.add("win");
+            }
+        });
+
+        if(answer !== ""){
+            gameInfo.innerText = `Winner Player -${answer}`;
+            newGameBtn.classList.add("active");
+            return;
+        }
+    };
 
 function handleClick(index){
     if(gameGrid[index] === "") {
         boxes[index].innerHTML = currentPlayer;
         gameGrid[index] = currentPlayer;
+        boxes[index].style.pointerEvents="none";
         //swap turn
         swapTurn();
         //check game over or not
         checkGameOver();
-    }
-}
+    };
+};
 
 boxes.forEach((box,index) => {
     box.addEventListener("click",()=>{
         handleClick(index);
-    })
+    });
 });
 
+newGameBtn.addEventListener("click",initGame);
